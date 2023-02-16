170063c587c69b9eba100f633880fed79c710074
tsy0103 <111820850+tsy0103@users.noreply.github.com>
2023-02-15 21:26:44 -0500

    Elvis

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
+const {expressjwt} = require('express-jwt')
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+var usersRouter = require('./routes/users');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,7 +12,13 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
-
+app.use(expressjwt({
+  algorithms: ['HS256'],
+  secret: 'secret000',  // 签名的密钥 或 PublicKey
+  credentialsRequired: true,  // 设置为 false 就不进行校验了，游客也可以访问
+}).unless({
+  path: ['/users/login',]  // 指定路径不经过 Token 解析
+}))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -22,7 +29,13 @@ if (process.env.NODE_ENV === 'production') {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
+app.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).send("invalid token...");
+  } else {
+    next(createError(404));
+  }
+});
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
